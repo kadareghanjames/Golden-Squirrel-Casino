e6b25111e067c5b212ba1b3ee02c719ec05e9481
gayl0003 <gayl0003@PCB-BCC-08>
2019-04-25 15:48:37 -0500

    paytable

--- GoldenSquirrelCasino/GoldenSquirrelCasino/MainWindow.xaml.cs
+++ GoldenSquirrelCasino/GoldenSquirrelCasino/MainWindow.xaml.cs
@@ -20,10 +20,15 @@ namespace GoldenSquirrelCasino
     /// </summary>
     public partial class MainWindow : Window
     {
-        string[] symbols = { @"https://pbs.twimg.com/profile_images/1087746536665243649/hO-51lBr.jpg", @"https://media.glassdoor.com/sqll/192597/golden-acorn-casino-squarelogo-1462369646556.png", @"https://media1.tenor.com/images/df0c7e27c55ba3ade8f0ebe78718be76/tenor.gif?itemid=7442174", @"http://clipart-library.com/data_images/306858.png", @"http://www.clker.com/cliparts/Z/Q/1/5/y/e/green-tree-md.png", @"https://cdn.shopify.com/s/files/1/2040/0303/products/Pretty_Paint_Art_Cartoon_Leaf_Leaves_-_Fall_11_96229170_200x200.jpg?v=1508507362", @"https://i.pinimg.com/originals/d9/70/89/d970898b49b3198b6f57575227aba224.jpg" };
+        string[] symbols = { "https://pbs.twimg.com/profile_images/1087746536665243649/hO-51lBr.jpg", @"https://media.glassdoor.com/sqll/192597/golden-acorn-casino-squarelogo-1462369646556.png", @"https://media1.tenor.com/images/df0c7e27c55ba3ade8f0ebe78718be76/tenor.gif?itemid=7442174", @"http://clipart-library.com/data_images/306858.png", @"http://www.clker.com/cliparts/Z/Q/1/5/y/e/green-tree-md.png", @"https://cdn.shopify.com/s/files/1/2040/0303/products/Pretty_Paint_Art_Cartoon_Leaf_Leaves_-_Fall_11_96229170_200x200.jpg?v=1508507362", @"https://i.pinimg.com/originals/d9/70/89/d970898b49b3198b6f57575227aba224.jpg" };
         public MainWindow()
         {
             InitializeComponent();
+
+
+
+
+
         }
 
         private void One_Selected(object sender, RoutedEventArgs e)
@@ -36,15 +41,16 @@ namespace GoldenSquirrelCasino
             for (int i = 1; i < 10; i++)
             {
                 Random r = new Random();
-                string image=r.Next(0, 8).ToString();
+                string image = r.Next(0, 8).ToString();
 
             }
         }
 
         private void BtnPayTable_Click(object sender, RoutedEventArgs e)
         {
-            Window win2 = new Window();
-            win2.Show();
+            Paytable pt = new Paytable();
+            pt.Show(); 
         }
     }
 }
+
